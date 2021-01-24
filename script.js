087e54d37229c3f40a3f7f77eb634ff24298e89d
Valera <desod777@gmail.com>
2021-01-24 18:37:50 +0300

    add if and for in js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
 
 const personalMovieDB = {
@@ -8,12 +10,27 @@ const personalMovieDB = {
     private: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов', ''),
-      b = prompt('На сколько оцените фильм?', ''),
-      c = prompt('Один из последних просмотренных фильмов', ''),
-      d = prompt('На сколько оцените фильм?', '');
+for(let i = 0; i < 2; i++){
+    const a = prompt('Один из последних просмотренных фильмов', ''),
+          b = prompt('На сколько оцените фильм?', '');
+    
+    if(a != null && b != null && a != '' && b != '' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if(personalMovieDB.count < 10){
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классичесий зритель');
+} else if (personalMovieDB.count >= 30){
+    console.log('Вы киноман');
+} else {
+    console.log('Error');
+}
 
-console.log(personalMovieDB);
+console.log(personalMovieDB.count);
