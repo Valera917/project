e86a8cdf5c235546a744b1d2f0801048ecab4a01
Valera <example@gmail.com>
2021-02-01 21:01:17 +0300

    add js for web

---
+++ front.js
@@ -0,0 +1,27 @@
+const circles = document.querySelectorAll('.circle'),
+      btns = document.querySelectorAll('button'),
+      wrap = document.querySelector('.wrapper'),
+      squares = wrap.querySelectorAll('.square'); // wrap как замена document
+
+btns.forEach(item => {
+    item.style.backgroundColor = 'pink';
+});
+
+const block = document.createElement('div');
+
+block.classList.add('black');
+
+wrap.append(block);
+// wrap.prepend(block);
+// squares[0].after(block);
+// squares[0].before(block);
+
+// circles[0].remove();
+
+// squares[0].replaceWith(circles[0]);
+
+// block.innerHTML = '<h1>Hello world</h1>';
+block.textContent = '<h1>Hello world</h2>';
+
+block.insertAdjacentHTML('afterend', '<h2>This is Valeraaaaaaaa</h2>')
+
